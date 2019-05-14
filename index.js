fa3fa3cbd98029d349998bb53a9b8673caf11f29
tyler knapp <tgknapp11@gmail.com>
2019-05-14 15:45:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ function logWhisper(string) {
   console.log(shout("HELLO"))
 }
 
-function sayHiToGrandma(string) {
-  if (whisper === "hello")  {
-    return("I can't hear you!")
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
     }
 }
